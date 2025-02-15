3ae8f57f2d5b24ea5d7d34eb30eb3a81a3423403
setu singh <setu9279@gmail.com>
2025-02-15 20:07:28 +0530

    delete

--- start.js
+++ start.js
@@ -1,13 +1,18 @@
-const account = 45456466;
-let account1 = "setu";
-var account2 = "4646";
-account3 = "mumbai";
-console.log(account, account1, account2, account3);
-// account=4;
-account1 = "abc";
-console.log(account1);
-account2 = "adaldfa";
-console.log(account2);
-account3 = 979;
-console.log(account3);
-//we use only for const and let keyword;
+// const account = 45456466;
+// let account1 = "setu";
+// var account2 = "4646";
+// account3 = "mumbai";
+// console.log(account, account1, account2, account3);
+// // account=4;
+// account1 = "abc";
+// console.log(account1);
+// account2 = "adaldfa";
+// console.log(account2);
+// account3 = 979;
+// console.log(account3);
+// 
+//we
+//  use only for const and let keyword;
+ //_____________________________________________________________________________________
+
+
