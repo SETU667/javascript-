2a3ab3a25d31f5a4af98535f469ed99f0e381ef0
setu singh <setu9279@gmail.com>
2025-02-15 20:47:39 +0530

    type of

---
+++ rule.js
@@ -0,0 +1,14 @@
+//emca 
+// alert("hello"); cannot be used in node js
+let Name="setu";
+let age=18;
+let age1=null; //null is an object
+//symbol--use to identify uniqueness;
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof age1);
+
+
+
+
+
