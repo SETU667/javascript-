d77cd3c96ff6ad25b580f3f0d76fb9f6e72d16d8
setu singh <setu9279@gmail.com>
2025-02-16 17:24:52 +0530

    conversion of datatype

--- rule.js
+++ rule.js
@@ -1,14 +1,29 @@
-//emca 
-// alert("hello"); cannot be used in node js
-let Name="setu";
-let age=18;
-let age1=null; //null is an object
-//symbol--use to identify uniqueness;
-console.log(typeof Name);
-console.log(typeof age);
-console.log(typeof age1);
+// //emca
+// // alert("hello"); cannot be used in node js
+// let Name="setu";
+// let age=18;
+// let age1=null; //null is an object
+// //symbol--use to identify uniqueness;
+// console.log(typeof Name);
+// console.log(typeof age);
+// console.log(typeof age1);
 
+// ________________________conversion________________________________________
 
+// const age = "Nan";
+// console.log(typeof age);
+// let a = Number(age);
+// console.log(typeof a);
+// console.log(a)
+// console.log(typeof NaN);
 
+//"asfa2323"=>nan
+//true = 1;
+// let a = "";
+// console.log(typeof a);
+// let b = Boolean(a);
+// console.log(b);
 
-
+// let a=77;
+// let string= String(a)
+// console.log(typeof string);
