d3c87e970358cd127b12bc5536527976b2ddef42
setu singh <setu9279@gmail.com>
2025-03-10 19:11:25 +0530

    addingh

---
+++ java.js
@@ -0,0 +1,40 @@
+// const parent = document.querySelector(".parent");
+// console.log(parent);
+// // console.log(parent.children[1].innerHTML);
+// for (let i = 0; i < parent.children.length; i++) {
+//   console.log(parent.children[i].innerHTML);
+// }
+// parent.children[3].style.color="red"
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+// console.log(document.querySelector(".day"))
+// console.log(document.querySelector(".day").parentElement);
+
+// const div=document.createElement("div")
+// console.log(div);
+// div.className='main'
+// div.id=Math.floor(Math.random()*10+1)
+// div.setAttribute("title","generatedTitle")
+// div.setAttribute("title","generatedTitle")
+// div.style.backgroundColor="blue"
+// div.style.color="orange"
+// div.style.padding="60px"
+// // div.innerHTML="setusingh"
+// div.appendChild(document.createTextNode("setu singh"))
+// document.body.appendChild(div)
+
+// function language(l) {
+//   const li = document.createElement("li");
+//   //   console.log(li);
+//   li.innerHTML = `${l}`;
+//   document.querySelector(".lan").appendChild(li);
+// }
+// language("setu");
+// function language(l) {
+//   const li = document.createElement("li");
+//   //   console.log(li);
+//   li.innerHTML = `${l}`;
+//   document.querySelector(".lan").appendChild(li);
+// }
+// language("setu");
--- start.js
+++ start.js
@@ -1,17 +1,17 @@
-// const account = 45456466;
-// let account1 = "setu";
-// var account2 = "4646";
-// account3 = "mumbai";
-// console.log(account, account1, account2, account3);
-// // account=4;
-// account1 = "abc";
-// console.log(account1);
-// account2 = "adaldfa";
+// // const account = 45456466;
+// // let account1 = "setu";
+// // var account2 = "4646";
+// // account3 = "mumbai";
+// // console.log(account, account1, account2, account3);
+// // // account=4;
+// // account1 = "abc";
+// // console.log(account1);
+// // account2 = "adaldfa";
 // console.log(account2);
-// account3 = 979;
-// console.log(account3);
-// 
-//  use only for const and let keyword;
- //_____________________________________________________________________________________
+// // account3 = 979;
+// // console.log(account3);
+// // 
+// //  use only for const and let keyword;
+//  //_____________________________________________________________________________________
 
 
